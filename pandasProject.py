9f65f527ff26a956fba718a99d93c6348a8ce1ec
Tirupathi123 <55702487+Tirupathi123@users.noreply.github.com>
2019-09-24 15:52:08 +0530

    Update pandasProject.py

--- pandasProject.py
+++ pandasProject.py
@@ -1,14 +1,13 @@
 import pandas as pd
 Data_frame=pd.read_csv('housing.csv')
 a=Data_frame
-print(a)                    #to read and get all the dataframe
-print(a.head(6))            #to get starting rows
-print(a.tail(4))            #to get last rows
-print(a[10:20])             #to get particular inBetween rows
-print(a.price.mean())       #to get mean of particular int column values #df['salary'].mean()
-print(a.bedrooms.dtype)                   #series      #prints type of the column
-print(a[['bedrooms','price']])            #data frame  #prints particular columns
-print(a.groupby('lotsize')[['price']].mean())
-print(a[a['price']<30000])
-print(a.dropna())
-print(a.describe())
+#print(a)                    #to read and get all the dataframe
+print('1 >',a.head(6))            #to get starting rows
+print('2 >',a.tail(4))            #to get last rows
+print('3 >',a[10:20])             #to get particular inBetween rows
+print('4 >',a.price.mean())       #to get mean of particular int column values #df['salary'].mean()
+print('5 >',a.bedrooms.dtype)                   #series      #prints type of the column
+print('6 >',a[['bedrooms','price']])            #data frame  #prints particular columns
+print('7 >',a.groupby('bedrooms')[['price']].mean())
+print('8 >',a[a['price']<25000])
+print('9 >',a.describe())
